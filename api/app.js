d199e6b6d1a848786b80438b32933e30c5fc1dc5
片田凌太 <aiirononeko@gmail.com>
2020-03-15 16:45:54 +0900

    s3のファイルを取得する実装を完了&TZの環境変数を設定

--- api/app.js
+++ api/app.js
@@ -43,83 +43,88 @@ function getFormattedOverTime(startTime, endTime) {
 // Lambdaハンドラ
 exports.handler = (event, context, callback) => {
 
-  const isComming = true;
+  const isComming = event.key; // trueなら出勤、falseなら退勤
 
-  XlsxPopulate.fromFileAsync('勤怠請負用(2020)_片田.xlsm')
-    .then((workbook) => {
+  const params = {
+    Bucket: 'autodakoku',
+    Key: '勤怠請負用(2020)_片田.xlsm',
+  }
 
-      console.log('処理を開始しました');
+  s3.getObject(params, (err, data) => {
+    console.log(data.Body);
 
-      let sheetNumber;
-      switch(date.getMonth() + 1) {
-        case 1: // 1月
-            sheetNumber = '01';
-            break;
-          case 2: // 2月
-            sheetNumber = '02';
-            break;
-          case 3: // 3月
-            sheetNumber = '03';
-            break;
-          case 4: // 4月
-            sheetNumber = '04';
-            break;
-          case 5: // 5月
-            sheetNumber = '05';
-            break;
-          case 6: // 6月
-            sheetNumber = '06';
-            break;
-          case 7: // 7月
-            sheetNumber = '07';
-            break;
-          case 8: // 8月
-            sheetNumber = '08';
-            break;
-          case 9: // 9月
-            sheetNumber = '09';
-            break;
-          case 10: // 10月
-            sheetNumber = '10';
-            break;
-          case 11: // 11月
-            sheetNumber = '11';
-            break;
-          case 12: // 12月
-            sheetNumber = '12';
-            break;
-      }
+    XlsxPopulate.fromDataAsync(data.Body)
+      .then((workbook) => {
 
-      console.log(`操作するシートは${sheetNumber}です`);
+        console.log('処理を開始しました');
 
-      const sheetName = workbook.sheet(sheetNumber);
-      const colNumber = (date.getDate() + 3).toString();
-
-      if (isComming) {
-        const startTimeValue = sheetName.cell(`D${colNumber}`).value(startTime);
-      } else {
-        const endTimeValue = sheetName.cell(`E${colNumber}`).value(endTime);
-        const restValue = sheetName.cell(`F${colNumber}`).value(rest);
-        const workTimeValue = sheetName.cell(`G${colNumber}`).value(workTime);
-        const overTimeValue = sheetName.cell(`H${colNumber}`).value(overTime);
-      }
-
-      workbook.toFileAsync('/tmp/勤怠請負用(2020)_片田.xlsm').then(() => {
-
-        const params = {
-          Bucket: 'autodakoku',
-          Key: '勤怠請負用(2020)_片田.xlsm',
-          Body: fs.readFileSync('/tmp/勤怠請負用(2020)_片田.xlsm')
+        let sheetNumber;
+        switch(date.getMonth() + 1) {
+          case 1: // 1月
+              sheetNumber = '01';
+              break;
+            case 2: // 2月
+              sheetNumber = '02';
+              break;
+            case 3: // 3月
+              sheetNumber = '03';
+              break;
+            case 4: // 4月
+              sheetNumber = '04';
+              break;
+            case 5: // 5月
+              sheetNumber = '05';
+              break;
+            case 6: // 6月
+              sheetNumber = '06';
+              break;
+            case 7: // 7月
+              sheetNumber = '07';
+              break;
+            case 8: // 8月
+              sheetNumber = '08';
+              break;
+            case 9: // 9月
+              sheetNumber = '09';
+              break;
+            case 10: // 10月
+              sheetNumber = '10';
+              break;
+            case 11: // 11月
+              sheetNumber = '11';
+              break;
+            case 12: // 12月
+              sheetNumber = '12';
+              break;
         }
-  
-        s3.putObject(params, (err, data) => {
-          if (err) { console.log(err); }
-          console.log('処理が完了しました');
-          context.done()
-        })
 
-      });
+        console.log(`操作するシートは${sheetNumber}です`);
+
+        const sheetName = workbook.sheet(sheetNumber);
+        const colNumber = (date.getDate() + 3).toString();
+
+        if (isComming) {
+          const startTimeValue = sheetName.cell(`D${colNumber}`).value(startTime);
+        } else {
+          const endTimeValue = sheetName.cell(`E${colNumber}`).value(endTime);
+          const restValue = sheetName.cell(`F${colNumber}`).value(rest);
+          const workTimeValue = sheetName.cell(`G${colNumber}`).value(workTime);
+          const overTimeValue = sheetName.cell(`H${colNumber}`).value(overTime);
+        }
+
+        workbook.toFileAsync('/tmp/勤怠請負用(2020)_片田.xlsm').then(() => {
+
+          params.Body = fs.readFileSync('/tmp/勤怠請負用(2020)_片田.xlsm');
+    
+          s3.putObject(params, (err, data) => {
+            if (err) { console.log(err); }
+            console.log('処理が完了しました');
+            context.done()
+          })
+
+        });
 
 
-  });
+    });
+  })
 }
